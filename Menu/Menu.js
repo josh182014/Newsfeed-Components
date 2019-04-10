80005df8f42ca4ea3194052ccbd99f1a050308a9
josh182014 <josh182014@gmail.com>
2019-04-10 14:50:37 -0600

    finished menu file

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,22 @@
 
-const toggleMenu = () => {
+const toggleMenu = (event) => {
   // Toggle the "menu--open" class on your menu refence. 
+  // if (menu.style["display"] === "block") {
+  //   menu.style["display"] = "none";
+  //   console.log('inside if statement')
+  //     return;
+  // }
+  // menu.style["display"] = "block";
+  // console.log('outside of if statement')
+  menu.classList.toggle('menu--open');
+  
 }
 
 // Start Here: Create a reference to the ".menu" class
-const menu;
+const menu = document.querySelector('.menu');
+
 // create a reference to the ".menu-button" class
-const menuButton;
+const menuButton = document.querySelector('.menu-button').addEventListener('click', function(event){
+  toggleMenu();
+})
 // Using your menuButton reference, add a click handler that calls toggleMenu
